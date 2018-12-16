c850b15840965eca09d8cd0cfd6e369d832cdb13
MobilityPune <amitkumar@AmitKumars-MacBook-Pro.local>
2018-12-16 12:42:51 +0530

    initial commit

---
+++ random/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  random
+//
+//  Created by MobilityPune on 16/12/18.
+//  Copyright © 2018 abc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
